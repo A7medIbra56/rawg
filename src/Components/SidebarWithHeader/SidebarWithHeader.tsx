8060ba5e0508b864a1c3d826790aaa48d94378d6
Ahmed Ibrahim <ahmedibrhiam512@gmail.com>
2023-12-03 04:56:05 +0200

    add mmore2

--- src/Components/SidebarWithHeader/SidebarWithHeader.tsx
+++ src/Components/SidebarWithHeader/SidebarWithHeader.tsx
@@ -151,7 +151,7 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
             </Flex>
           </Box>
         ))}
-        <h3>Genres</h3>
+        <Text ml={6} pt={2} fontSize={"3xl"} color={"white"}>Genres</Text>
         {LinkItemsGenres.map((item) => (
           <Link style={{ textDecoration: 'none' }} key={item.name} to={`${item.to}`}>
             <Box>
@@ -196,10 +196,10 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
       <Text
         display={{ base: "flex", md: "none" }}
         fontSize="2xl"
+        color={"white"}
         fontFamily="monospace"
         fontWeight="bold"
-      >
-        Logo
+      >RAWG
       </Text>
 
       <HStack spacing={{ base: "0", md: "6" }}>
@@ -210,46 +210,6 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
           icon={<FiBell />}
         />
         <Flex alignItems={"center"}>
-          <Menu>
-            <MenuButton
-              py={2}
-              transition="all 0.3s"
-              _focus={{ boxShadow: "none" }}
-            >
-              <HStack>
-                <Avatar
-                  size={"sm"}
-                  src={
-                    "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-                  }
-                />
-                <VStack
-                  display={{ base: "none", md: "flex" }}
-                  alignItems="flex-start"
-                  spacing="1px"
-                  ml="2"
-                >
-                  <Text fontSize="sm">Justina Clark</Text>
-                  <Text fontSize="xs" color="#151515">
-                    Admin
-                  </Text>
-                </VStack>
-                <Box display={{ base: "none", md: "flex" }}>
-                  <FiChevronDown />
-                </Box>
-              </HStack>
-            </MenuButton>
-            <MenuList
-              bg={useColorModeValue("#151515", "#151515")}
-              borderColor={useColorModeValue("#151515", "#151515")}
-            >
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Billing</MenuItem>
-              <MenuDivider />
-              <MenuItem>Sign out</MenuItem>
-            </MenuList>
-          </Menu>
         </Flex>
       </HStack>
     </Flex>
